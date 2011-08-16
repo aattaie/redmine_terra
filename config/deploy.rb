91c128b356a5515aceef3632b71b246ba4bfcdad
Tracey Manley <t.manley@terrabase.co.uk>
2011-08-16 18:30:28 +0100

    Add Capistrano configuration, manley theme,and plugin submodules

---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+set :application, "redmine"
+set :repository,  "git@s15401259.onlinehome-server.info:redmine.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "82.165.130.71"                          # Your HTTP server, Apache/etc
+role :app, "82.165.130.71"                          # This may be the same as your `Web` server
+role :db,  "82.165.130.71", :primary => true # This is where Rails migrations will run
+
+set :deploy_via, :remote_cache
+set :use_sudo, false
+set :user, "deploy"
+set :branch, "terrabase"
+set :deploy_to, "/home/www/redmine"
+set :rails_env, "production"
+
+ssh_options[:keys] = "/home/tracey/.ssh/id_rsa"
+
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+after 'deploy:update_code', 'deploy:symlink_db'
